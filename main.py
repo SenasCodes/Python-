93c9533df871c15f56d37f6a301cc6ef98d2e05a
Senaz <gongasena2001@gmail.com>
2024-01-25 00:40:19 +0000

    Mudança pc

--- main.py
+++ main.py
@@ -11,6 +11,6 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCmbjhj76wdwjhljffhfuigmbfre,llkjffdgnghgjkenkgfgevbfekhbvrrbngjijnbharm')
+    print_hi('PyCmbjhj76wjhdwjhljffhfuigmbfre,llkjffdgnghgjkenkgfgevbfekhbvrrbngjijnbharm')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
